ee633994e22234bf7b5bb35ec10540fae82d106a
Vasilis Tsimpragakis <v.tsimpragakis@apt.gr>
2021-04-21 16:23:49 +0300

    initialization

---
+++ src/main/java/com/tsimpra/filesmanagment/FilesmanagmentApplication.java
@@ -0,0 +1,13 @@
+package com.tsimpra.filesmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesmanagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesmanagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsimpra/filesmanagment/FilesmanagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsimpra.filesmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesmanagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
